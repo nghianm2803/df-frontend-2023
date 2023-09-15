70dcd3cea60de2a62d634f7b2e42f0cb675a8898
nghia <nghianm2803@gmail.com>
2023-09-15 17:40:03 +0700

    first commit

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -1,1 +1,175 @@
 // Your JS code goes here
+
+let books = JSON.parse(localStorage.getItem("books")) || [
+  { id: 1, name: "Refactoring", author: "Martin Fowler", topic: "Programming" },
+  {
+    id: 2,
+    name: "Designing Data-Intensive Applications",
+    author: "Martin Kleppmann",
+    topic: "Database",
+  },
+  { id: 3, name: "The Phoenix Project", author: "Gene Kim", topic: "DevOps" },
+];
+
+// Store LocalStorage
+function loadBooksFromLocalStorage() {
+  const storedBooks = JSON.parse(localStorage.getItem("books"));
+  if (storedBooks) {
+    books = storedBooks;
+    populateTable();
+  }
+}
+
+loadBooksFromLocalStorage();
+
+// Modal
+const addModal = document.getElementById("addBookModal");
+const deleteModal = document.getElementById("deleteBookModal");
+const addBtn = document.getElementById("addBtn");
+const cancelBtn = document.getElementById("cancelBtn");
+const deleteBtnConfirm = document.getElementById("deleteBtnConfirm");
+const closeAddModal = document.querySelector("#addBookModal .close");
+const closeDeleteModal = document.querySelector("#deleteBookModal .close");
+
+closeAddModal.addEventListener("click", function () {
+  document.getElementById("addBookModal").style.display = "none";
+});
+
+closeDeleteModal.addEventListener("click", function () {
+  document.getElementById("deleteBookModal").style.display = "none";
+});
+
+cancelBtn.onclick = function () {
+  document.getElementById("deleteBookModal").style.display = "none";
+};
+
+addBtn.onclick = function () {
+  addModal.style.display = "block";
+};
+
+window.onclick = function (event) {
+  if (event.target == addModal) {
+    addModal.style.display = "none";
+  }
+  if (event.target == deleteModal) {
+    deleteModal.style.display = "none";
+  }
+};
+
+// Function to add book
+function addBook() {
+  const nameInput = document.querySelector('input[name="name"]');
+  const authorInput = document.querySelector('input[name="author"]');
+  const topicSelect = document.querySelector("select");
+  const errorMessage = document.getElementById("error-message");
+
+  // Validate input
+  if (!nameInput.value) {
+    errorMessage.textContent = "Please enter name of the book";
+    return;
+  }
+  if (!authorInput.value) {
+    errorMessage.textContent = "Please enter author of the book";
+    return;
+  }
+
+  const name = nameInput.value;
+  const author = authorInput.value;
+  const topic = topicSelect.value;
+
+  const bookId = books.length + 1;
+  const newBook = { id: bookId, name, author, topic };
+  books.push(newBook);
+  localStorage.setItem("books", JSON.stringify(books));
+
+  nameInput.value = "";
+  authorInput.value = "";
+  errorMessage.textContent = "";
+
+  populateTable();
+
+  addModal.style.display = "none";
+}
+
+const createButton = document.getElementById("createBtn");
+createButton.addEventListener("click", addBook);
+
+// Function to search book
+function searchBooks() {
+  const searchInput = document
+    .querySelector('input[name="search"]')
+    .value.toLowerCase()
+    .trim();
+  const filteredBooks = books.filter((book) =>
+    book.name.toLowerCase().includes(searchInput)
+  );
+  console.log("filter books:", filteredBooks);
+  populateTable(filteredBooks);
+}
+
+const searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  searchBooks();
+});
+
+// Function to show book
+function populateTable(filteredData) {
+  const tbody = document.querySelector("tbody");
+
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+
+  const dataToRender = filteredData || books;
+
+  dataToRender.forEach((item) => {
+    const row = document.createElement("tr");
+    const name = document.createElement("td");
+    const author = document.createElement("td");
+    const topic = document.createElement("td");
+    const deleteBtn = document.createElement("td");
+    const deleteLink = document.createElement("a");
+    const deleteText = document.getElementById("confirm-message");
+
+    name.textContent = item.name;
+    row.appendChild(name);
+    author.textContent = item.author;
+    row.appendChild(author);
+    topic.textContent = item.topic;
+    row.appendChild(topic);
+
+    deleteLink.textContent = "Delete";
+    deleteLink.classList.add("deleteLink");
+
+    // Open delete modal
+    deleteLink.addEventListener("click", () => {
+      const deleteBookId = item.id;
+      deleteModal.style.display = "block";
+
+      deleteText.innerHTML = `Do you want to delete <b>${item.name}</b> book?`;
+      // Click on delete confirm
+      deleteBtnConfirm.addEventListener("click", () => {
+        deleteBook(deleteBookId);
+      });
+    });
+
+    deleteBtn.appendChild(deleteLink);
+    row.appendChild(deleteBtn);
+
+    tbody.appendChild(row);
+  });
+}
+
+// Function to delete book
+function deleteBook(bookId) {
+  const index = books.findIndex((item) => item.id === bookId);
+  if (index !== -1) {
+    books.splice(index, 1);
+    populateTable();
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+  deleteModal.style.display = "none";
+}
+
+populateTable();
