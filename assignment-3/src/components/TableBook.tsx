b961df5948c189205533ca5185fd6934045eeb26
nghia <nghianm2803@gmail.com>
2023-09-25 17:25:44 +0700

    test husky

--- assignment-3/src/components/TableBook.tsx
+++ assignment-3/src/components/TableBook.tsx
@@ -4,7 +4,7 @@ import DeleteBook from './DeleteBook'
 import Pagination from './Pagination'
 import EmptyData from './EmptyData'
 
-var a sad
+var a sadsdasd
 
 interface TableBookProps {
   books: IBook[]
