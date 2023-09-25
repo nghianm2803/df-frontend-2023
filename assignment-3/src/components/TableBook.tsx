d55fca64a1b5852f7b0d974c450c19013e1c566e
nghia <nghianm2803@gmail.com>
2023-09-25 22:46:01 +0700

    test husky attemp 17

--- assignment-3/src/components/TableBook.tsx
+++ assignment-3/src/components/TableBook.tsx
@@ -4,7 +4,7 @@ import DeleteBook from './DeleteBook'
 import Pagination from './Pagination'
 import EmptyData from './EmptyData'
 
-var ashello aasdcsasa sa
+var asdsd
 
 interface TableBookProps {
   books: IBook[]
