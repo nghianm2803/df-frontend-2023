621f66098090ec47f5413e20d744e307d5960a6c
nghia <nghianm2803@gmail.com>
2023-09-28 10:46:55 +0700

    WIP toggle theme

--- assignment-4/src/components/EmptyData.tsx
+++ assignment-4/src/components/EmptyData.tsx
@@ -4,9 +4,15 @@ import EmptyDataImage from '../assets/emptydata.svg'
 function EmptyData() {
   return (
     <div className="flex flex-col align-middle justify-center">
-      <Image className='m-auto w-96 h-80' src={EmptyDataImage} alt="emptydata" />
-      <p className='text-center'>No record available. Try another page or add a new book</p>
-        {/* View detail -> Delete -> Navigate to home */}
+      <Image
+        className="m-auto w-96 h-80"
+        src={EmptyDataImage}
+        alt="emptydata"
+      />
+      <p className="text-center">
+        No record available. Try another page or add a new book
+      </p>
+      {/* View detail -> Delete -> Navigate to home */}
     </div>
   )
 }
--- assignment-4/src/layouts/MainHeader.tsx
+++ assignment-4/src/layouts/MainHeader.tsx
@@ -2,29 +2,33 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import Avatar from '../assets/avatar.png'
+import ToggleTheme from '../components/ToggleTheme'
 
 const MainHeader = () => {
   return (
-    <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] shadow-lg h-fit border-b border-zinc-300 py-2">
+    <div className="fixed inset-x-0 top-0 bg-white z-[10] custom-shadow h-fit py-2">
       <div className="flex items-center justify-between h-full gap-2 mx-auto max-w-7xl">
-        <Link href="/" className="flex items-center gap-2">
+        <Link href="/" className="flex items-center">
           <p className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white">
             Bookstore
           </p>
         </Link>
-        <Link
-          href="https://github.com/nghianm2803"
-          className="flex items-center float-right"
-          target="_blank"
-        >
-          <Image
-            src={Avatar}
-            alt="Avatar"
-            className="inline-block h-10 w-10 rounded-full ring-2 ring-white"
-            priority
-          />
-          <div className="flex items-center"> Du Xa Xiu</div>
-        </Link>
+        <div className="flex align-middle float-right gap-2">
+          <ToggleTheme />
+          <Link
+            href="https://github.com/nghianm2803"
+            className="flex items-center float-right gap-2"
+            target="_blank"
+          >
+            <Image
+              src={Avatar}
+              alt="Avatar"
+              className="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+              priority
+            />
+            <div className="flex items-center"> Du Xa Xiu</div>
+          </Link>
+        </div>
       </div>
     </div>
   )
