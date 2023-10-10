8d5bef1fc423891ab98fb94d9abc39006fbc396b
nghia <nghianm2803@gmail.com>
2023-10-10 20:19:34 +0700

    refactor code

--- assignment-5/src/app/page.tsx
+++ assignment-5/src/app/page.tsx
@@ -1,16 +1,3 @@
-// 'use client'
-
-// import React from 'react'
-// import MainLayout from '../layouts/MainLayout'
-
-// export default function Home() {
-//   return (
-//     <div className="flex flex-col justify-between min-h-screen dark:bg-slate-800">
-//       <MainLayout />
-//     </div>
-//   )
-// }
-
 'use client'
 
 import React from 'react'
