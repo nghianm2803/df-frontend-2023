00fa6825857307e45e46f58a37c6c157c169e0dd
nghia <nghianm2803@gmail.com>
2023-10-13 10:38:28 +0700

    refactor code

--- assignment-6/src/app/apiService.tsx
+++ assignment-6/src/app/apiService.tsx
@@ -9,7 +9,7 @@ apiService.interceptors.request.use(
     // console.log('Start Request', request)
     return request
   },
-  function (error) {
+  function RequestError(error) {
     // console.log('REQUEST ERROR', error)
     return Promise.reject(error)
   },
@@ -20,7 +20,7 @@ apiService.interceptors.response.use(
     // console.log('Response', response)
     return response
   },
-  function (error) {
+  function ResponseError(error) {
     // console.log('RESPONSE ERROR', error)
     const message = error.response?.data?.message || 'Unknown error'
     const rejectionError = new Error(message)
